69d3e6ce774843afac33617dd78a9898a30c5fb0
xxc <x007007007@hotmail.com>
2017-09-03 01:13:45 +0800

    vue update

--- src/supervisor_monitor/settings.py
+++ src/supervisor_monitor/settings.py
@@ -37,18 +37,32 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'supervisor_monitor'
+    'supervisor_monitor',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_REGEX_WHITELIST = ".*"
+
+CORS_ALLOW_METHODS = (
+    'DELETE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+    'FETCH',
+)
 
 ROOT_URLCONF = 'supervisor_monitor.web_urls'
 
---
+++ vue/src/pages/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="supervisor_list">
+    <button v-on:click="refresh">refresh</button>
+    <ul>
+      <li v-for="sinfo in supervisor_list">
+          <span class="identification">{{ sinfo.identification }}</span>
+          <span class="ip">{{ sinfo.ip }}</span>
+          <span class="pid">{{ sinfo.pid }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+
+<script>
+  export default {
+    state: {
+      supervisor_list: null
+    },
+    data: () => {
+      return {
+        supervisor_list: []
+      }
+    },
+    methods: {
+      async refresh () {
+        this.supervisor_list = (await (await fetch('http://127.0.0.1:8000/monitor/api/supervisor/')).json()).data
+      }
+    },
+    mounted: function () {
+      this.refresh()
+    }
+  }
+</script>
+
+
+<style lang="stylus">
+
+</style>
